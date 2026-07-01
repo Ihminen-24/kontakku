4e81f963fa48c32e7183e20c09281b77c95572ff
Ihminen-24 <magerlabs@gmail.com>
2026-07-01 23:31:42 +0700

    Code Frontend yang baru

---
+++ Frontend(New).py
@@ -0,0 +1,78 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+import Backend
+
+# Window
+window = tk.Tk()
+window.title("Kontakku V0.1")
+window.geometry("600x400")
+window.resizable(False, False)
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+# Frame
+frame_kanan = ttk.Frame(window)
+frame_kanan.grid(row=0, column=1, sticky="nsew")
+frame_kanan.rowconfigure(0, weight=1)
+frame_kanan.columnconfigure(0, weight=1)
+frame_kiri = ttk.Frame(window)
+frame_kiri.grid(row=0, column=0, sticky="nsew")
+# Label
+label_nama = ttk.Label(frame_kiri, text="Nama")
+label_nama.grid(row=0, column=0)
+label_nomor = ttk.Label(frame_kiri, text="Nomor")
+label_nomor.grid(row=2, column=0)
+# Entry
+input_nama = ttk.Entry(frame_kiri)
+input_nama.grid(row=1, column=0, pady=5, padx=10)
+input_nomor = ttk.Entry(frame_kiri)
+input_nomor.grid(row=3, column=0, pady=5, padx=10)
+# Treeview
+list_kontak = ttk.Treeview(frame_kanan, columns=("Nama", "Nomor"))
+list_kontak.heading("Nama", text="Nama Kontak")
+list_kontak.heading("Nomor", text="Nomor Kontak")
+list_kontak.grid(row=0, column=0, padx=10, pady=10)
+list_kontak.column("#0", width=0, stretch=False)
+list_kontak.column("Nama", width=150)
+list_kontak.column("Nomor", width=150)
+
+
+# Function Tambahkan Kontak
+def tambah_kontak_gui():
+    nama = input_nama.get()
+    nomor = input_nomor.get()
+
+    if nama == "" or nomor == "":
+        messagebox.showerror(title="Error", message="Harus di Isi")
+        return
+    list_kontak.insert("", tk.END, values=(nama, nomor))
+    input_nama.delete(0, tk.END)
+    input_nomor.delete(0, tk.END)
+
+
+# Button Input Kontak
+button_input = ttk.Button(
+    frame_kiri, text="Tambahkan ke Kontakku", command=tambah_kontak_gui
+)
+button_input.grid(row=4, column=0, pady=5, padx=10)
+
+
+# Function Hapus Kontak
+def hapus_kontak_gui():
+    item_terpilih = list_kontak.selection()
+
+    if not item_terpilih:
+        messagebox.showwarning(
+            title="Warning", message="Pilih Kontak yang ingin dihapus"
+        )
+        return
+    list_kontak.delete(item_terpilih)
+
+
+# Button Hapus Kontak
+button_hapus = ttk.Button(
+    frame_kiri, text="Hapus dari Kontakku", command=hapus_kontak_gui
+)
+button_hapus.grid(row=5, column=0, pady=5, padx=10)
+
+window.mainloop()
